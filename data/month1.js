5b859785271ef3423b2d3d2edd010a347c5cbc2c
KK13 <kk13@KK13ui-MacBookAir.local>
2026-03-22 10:28:07 +0900

    Refactor: Separate JSON data into data/month.js files for stability

---
+++ data/month1.js
@@ -0,0 +1,2 @@
+window.dayData = window.dayData || {};
+// Month 1 (Day 1 - 30) data will be added here
---
+++ data/month2.js
@@ -0,0 +1,2 @@
+window.dayData = window.dayData || {};
+// Month 2 (Day 31 - 60) data will be added here
